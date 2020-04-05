1b65cf5e7572927a53234897f7f94bc0ec7004b3
Julien Bourdeau <julien@sigerr.org>
2020-04-05 12:06:05 +0200

    Handle casts for posts

--- src/Post.php
+++ src/Post.php
@@ -11,11 +11,34 @@ class Post extends Model
 
     protected $keyType = 'string';
 
+    protected $casts = [
+        'featured' => 'boolean',
+        'send_email_when_published' => 'boolean',
+        'reading_time' => 'int',
+        'tags' => 'array',
+        'primary_tag' => 'array',
+        'authors' => 'array',
+        'primary_author' => 'array',
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+        'published_at' => 'datetime',
+    ];
+
     public function getRows()
     {
-        return app(GhostClient::class)->posts([
+        $rows = app(GhostClient::class)->posts([
             'limit' => 'all',
             'include' => 'authors,tags',
         ])['posts'];
+
+        foreach ($rows as &$row) {
+            foreach ($this->casts as $attribute => $type) {
+                if ($type === 'array') {
+                    $row[$attribute] = json_encode($row[$attribute]);
+                }
+            }
+        }
+
+        return $rows;
     }
 }
--- tests/PostModelTest.php
+++ tests/PostModelTest.php
@@ -2,6 +2,7 @@
 
 namespace Julienbourdeau\LaravelGhostConnector\Tests;
 
+use Carbon\Carbon;
 use Julienbourdeau\LaravelGhostConnector\GhostClient;
 use Julienbourdeau\LaravelGhostConnector\LaravelGhostConnectorServiceProvider;
 use Julienbourdeau\LaravelGhostConnector\Post;
@@ -12,7 +13,7 @@ use Orchestra\Testbench\TestCase;
  * @internal
  * @coversNothing
  */
-class ModelTest extends TestCase
+class PostModelTest extends TestCase
 {
     /** @test */
     public function postsAreRetrievedAsModelCollection()
@@ -53,6 +54,39 @@ class ModelTest extends TestCase
 
         $this->assertEquals('5e80568dc50444aef65bea46', Post::first()->id);
         $this->assertEquals('d28c25c8-59b6-4a9f-ae80-8c0db3f82255', Post::find('5e80568dc50444aef65bea3e')->uuid);
+    }
+
+    /** @test */
+    public function attributesAreCasted()
+    {
+        $this->instance(GhostClient::class, Mockery::mock(GhostClient::class, function ($mock) {
+            $mock->shouldReceive('posts')->andReturn($this->posts());
+        }));
+
+        // array types needs to be json_encoded when returned from getRows
+        $rows = (new Post)->getRows();
+        $first = $rows[0];
+        $this->assertIsString($first['tags']);
+        $this->assertIsString($first['primary_tag']);
+        $this->assertIsString($first['authors']);
+        $this->assertIsString($first['primary_author']);
+        $this->assertIsArray(json_decode($first['tags'], true));
+        $this->assertIsArray(json_decode($first['primary_tag'], true));
+        $this->assertIsArray(json_decode($first['authors'], true));
+        $this->assertIsArray(json_decode($first['primary_author'], true));
+
+        $this->assertIsBool(Post::first()->featured);
+        $this->assertIsBool(Post::first()->send_email_when_published);
+        $this->assertIsInt(Post::first()->reading_time);
+
+        $this->assertInstanceOf(Carbon::class, Post::first()->created_at);
+        $this->assertInstanceOf(Carbon::class, Post::first()->updated_at);
+        $this->assertInstanceOf(Carbon::class, Post::first()->published_at);
+
+        $this->assertIsArray(Post::first()->tags);
+        $this->assertIsArray(Post::first()->primary_tag);
+        $this->assertIsArray(Post::first()->authors);
+        $this->assertIsArray(Post::first()->primary_author);
     }
 
     protected function getPackageProviders($app)
